28a1d7746a968301ddaa8d0f4ec410d2f25c9beb
ibrahim badr <ibrahimbdr@outlook.com>
2023-04-22 16:14:29 +0200

    updating find shops page style

--- src/pages/bookingShop.js
+++ src/pages/bookingShop.js
@@ -127,52 +127,53 @@ const ShopSelection = () => {
 
   return (
     <div className="p-6 h-screen bg-gray-100 overflow-auto">
-      <h1 className="text-center text-5xl font-extrabold mb-8 text-gray-900">
-        Choose a Shop
-      </h1>
+      <div className="flex flex-col h-full justify-center items-center">
+        <h1 className="text-center text-5xl font-extrabold mb-8 text-gray-900">
+          Choose a Shop
+        </h1>
 
-      <div className="flex items-center justify-center mb-4">
-        <input
-          type="text"
-          placeholder="Search shops"
-          value={searchQuery}
-          onChange={handleSearchQueryChange}
-          className="py-2 px-4 rounded-md border-gray-300 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-        <FaSearch className="ml-2 text-gray-500" />
-      </div>
+        <div className="flex items-center justify-center mb-4">
+          <input
+            type="text"
+            placeholder="Search shops"
+            value={searchQuery}
+            onChange={handleSearchQueryChange}
+            className="py-2 px-4 rounded-md border-gray-300 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+          <FaSearch className="ml-2 text-gray-500" />
+        </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center">
-        {currentShops.map((shop) => (
-          <div
-            key={shop.id}
-            className={`max-w-sm rounded overflow-hidden shadow-lg cursor-pointer ${
-              selectedShop?.id === shop.id ? "ring-2 ring-indigo-500" : ""
-            }`}
-            onClick={() => handleShopSelection(shop)}
-          >
-            <img className="w-full" src={shop.imageUrl} alt={shop.name} />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{shop.name}</div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center">
+          {currentShops.map((shop) => (
+            <div
+              key={shop.id}
+              className={`max-w-sm rounded overflow-hidden shadow-lg cursor-pointer ${
+                selectedShop?.id === shop.id ? "ring-2 ring-indigo-500" : ""
+              }`}
+              onClick={() => handleShopSelection(shop)}
+            >
+              <img className="w-full" src={shop.imageUrl} alt={shop.name} />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{shop.name}</div>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <nav className="flex justify-center mt-4 bg-gray-100">
-        <button
-          onClick={handlePrevClick}
-          className={`${
-            currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
-          } bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded-l-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
-          disabled={currentPage === 1}
-        >
-          Prev
-        </button>
-        {pageNumbers.map((number) => (
+          ))}
+        </div>
+        <nav className="flex justify-center mt-4 bg-gray-100">
           <button
-            key={number}
-            onClick={(event) => handlePageChange(event, number)}
-            className={`
+            onClick={handlePrevClick}
+            className={`${
+              currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
+            } bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded-l-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+            disabled={currentPage === 1}
+          >
+            Prev
+          </button>
+          {pageNumbers.map((number) => (
+            <button
+              key={number}
+              onClick={(event) => handlePageChange(event, number)}
+              className={`
             ${
               currentPage === number
                 ? "bg-indigo-600 text-white"
@@ -186,29 +187,30 @@ const ShopSelection = () => {
             focus:ring-offset-2
             focus:ring-indigo-500
           `}
+            >
+              {number}
+            </button>
+          ))}
+          <button
+            onClick={handleNextClick}
+            className={`${
+              currentPage === pageNumbers.length
+                ? "opacity-50 cursor-not-allowed"
+                : ""
+            } bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded-r-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+            disabled={currentPage === pageNumbers.length}
           >
-            {number}
+            Next
           </button>
-        ))}
-        <button
-          onClick={handleNextClick}
-          className={`${
-            currentPage === pageNumbers.length
-              ? "opacity-50 cursor-not-allowed"
-              : ""
-          } bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded-r-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
-          disabled={currentPage === pageNumbers.length}
-        >
-          Next
-        </button>
-      </nav>
-      {selectedShop && (
-        <Link to={`/booking-start`}>
-          <button className="bg-indigo-600 hover:bg-indigo-700 text-white text-xl font-medium py-4 px-8 rounded-full mt-8 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Continue
-          </button>
-        </Link>
-      )}
+        </nav>
+        {selectedShop && (
+          <Link to={`/booking-start`}>
+            <button className="bg-indigo-600 hover:bg-indigo-700 text-white text-xl font-medium py-4 px-8 rounded-full mt-8 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Continue
+            </button>
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
